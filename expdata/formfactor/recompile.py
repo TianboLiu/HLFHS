9a2bfafca2c9ec3ffff43c022e08e259428ca76a
User <user@UsermatoMacBook-Pro-2.local>
2018-10-23 22:36:44 -0400

    recompile

--- expdata/formfactor/recompile.py
+++ expdata/formfactor/recompile.py
@@ -75,6 +75,14 @@ def Load(idx):
         d1['G_Ep/G_D'] = d0['G_Ep'].values.flatten() / G_D(d1['Q2'])
         d1['error'] = ((d0['err_stat'].values**2 + d0['err_syst'].values**2)**0.5).flatten() * d1['G_Ep/G_D']
         d2 = pd.DataFrame(data=d1)
+    elif idx in [78,]:
+        d1 = {}
+        d1['Q2'] = d0['Q2'].values.flatten()
+        if d0['Q2'].columns[0] == 'fm^-2':
+            d1['Q2'] = fm2GeV(d1['Q2'], 2)
+        d1['G_Ep/G_D'] = d0['G_Ep'].values.flatten() / G_D(d1['Q2'])
+        d1['error'] = ((d0['err_stat']**2 + d0['err_syst']**2)**0.5).values.flatten() / G_D(d1['Q2'])
+        d2 = pd.DataFrame(data=d1)
     elif idx in [76,]:
         d1 = {}
         d1['Q2'] = d0['Q2'].values.flatten()
